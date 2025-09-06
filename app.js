8bba5a910caa6d4aa403db4595b51d2d1a9b716d
Umaima Salam <umaimasalam2003@gmail.com>
2025-09-06 20:09:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-// // confirm("Do You Want to give Us Information?")
-// // let pname = prompt("Enter Your Name: ")
-// // let pfatherName = prompt("Enter Your Father Name: ")
-// // let pRollNo = prompt("Enter Your Roll No: ") 
-// // let pcourse = prompt("Enter Your Course Name: ");
-// // let name = document.getElementById("name")
-// // let fatherName = document.getElementById('fatherName');
-// // let RollNo = document.getElementById('RollNo');
-// // let course = document.getElementById('course');
-// // name.value = pname;
-// // fatherName.value = pfatherName
-// // RollNo.value = pRollNo;
-// // course.value= pcourse;
-
-// let inputName = document.getElementById("name").value;
-// let inputFatherName = document.getElementById("fatherName").value;
-// let inputRollNo = document.getElementById("RollNo").value;
-// let generatebtn = document.getElementById("generatebtn");
-// generatebtn.addEventListener("click" , function(){
-//     document.writeln(`<h1> ID Card </h1> <h3> Name: </h3> ${inputName} <h3> Father Name: </h3> ${inputFatherName}  <h3> Roll No: ${inputRollNo}`  )
-// })
-
-
 let name = document.getElementById("name");
 let fathername = document.getElementById("fathername");
 let email = document.getElementById("email");
@@ -34,6 +11,8 @@ let inputimage = document.getElementById("inputimage");
 let inputname = document.getElementById("inputname");
 let inputfathername = document.getElementById("inputfathername");
 let inputemail = document.getElementById("inputemail");
+let inputnumber = document.getElementById("inputnumber");
+inputnumber.value=Math.floor(Math.random()*100000)+900000;
 button.addEventListener('click' , function(){
 hidden.style.display= "block"
 inputname.value= name.value;
